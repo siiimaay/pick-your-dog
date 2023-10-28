1756ee60e11d8a49f566d6b4186621a292984880
simay <simay.ekici@grubtech.com>
2023-10-28 14:46:35 +0300

    update .gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(
+        title: "Pick your dog",
+      ),
     );
   }
 }
